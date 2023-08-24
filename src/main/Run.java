2a3c04c7937cb82f1554d02f6093f253a79cb497
mprocz <marcosprocykevicz@gmail.com>
2023-08-24 12:27:27 -0300

    finalizado

--- src/main/Run.java
+++ src/main/Run.java
@@ -7,7 +7,6 @@ public class Run {
     public static void main(String[] args){
         System.out.println(ConsoleColors.RED_BRIGHT + "Iniciando dispositivo...\n" + ConsoleColors.RESET);
         CPU cpu = new CPU();
-
         int row = 0;
         int column = 0;
         cpu.startProcess(row, column, 1);
